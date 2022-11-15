771688b2737aef80f92540512427f7b661e9209d
Ayush-Dabas <aydbs23@gmail.com>
2022-11-15 18:17:00 +0530

    some random changes

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,15 @@
 import './App.css';
 import NavBar from './components/NavBar';
+import TextForm from './components/TextForm';
 
 function App() {
   return (
     <>
     <NavBar title="HO HO HO" aboutText="KONO DIO DA"/>
+    <div className="container">
+    <TextForm/>
+
+    </div>
     </>
   );
 }
